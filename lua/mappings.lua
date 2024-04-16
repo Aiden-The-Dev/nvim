33479c65c7cd824a092ab9d1aeaa9551bb209e7e
Aiden <aidenthedev@gmail.com>
2024-04-16 05:46:17 +0300

    Mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,36 @@
 require "nvchad.mappings"
 
--- add yours here
-
+local nomap = vim.keymap.del
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+nomap("n", "<leader>rn")
+map("n", "<leader>nr", "<cmd>set rnu!<CR>", { desc = "Toggle Relative number" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+nomap("n", "<leader>n")
+map("n", "<leader>nl", "<cmd>set nu!<CR>", { desc = "Toggle Line number" })
+
+nomap("n", "<leader>ch")
+map("n", "<leader>nm", "<cmd>NvCheatsheet<CR>", { desc = "Toggle NvCheatsheet" })
+
+----
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<C-v>", "<cmd>p<CR>", { desc = "Paste" })
+
+map("n", "s", "<cmd>w<CR>", { desc = "Save File" })
+map("n", "ss", "<cmd>wall<CR>", { desc = "Save Project" })
+map("n", "<leader>s", "<cmd>wall<CR>", { desc = "Save Project" })
+map("n", "<leader>w", "<cmd>wall<CR>", { desc = "Save Project" })
+map("n", "<leader>c", "<cmd>close<CR>", { desc = "Close Buffer" })
+
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree Explorer" })
+map("n", "<leader>o", function ()
+  if vim.bo.filetype == "NvimTree" then
+    vim.cmd.wincmd "p"
+  else
+    vim.cmd "NvimTreeFocus"
+  end
+end, { desc = "Toggle NvimTree Focus" })
+
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
