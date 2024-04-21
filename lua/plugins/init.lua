01fb053a56d323b5a2cb02b10d3377a578a22b54
Aiden <155806565+Aiden-The-Dev@users.noreply.github.com>
2024-04-21 03:52:37 +0300

    Plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,11 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
   },
-
 
   {
     "neovim/nvim-lspconfig",
@@ -16,14 +15,40 @@ return {
     end,
   },
 
+  -- { "williamboman/mason-lspconfig.nvim" },
+  -- { "jay-babu/mason-null-ls.nvim" },
+  -- { "jay-babu/mason-nvim-dap.nvim" },
   {
   	"williamboman/mason.nvim",
   	opts = {
   		ensure_installed = {
   			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier"
+  			"html-lsp", "css-lsp" , "prettier",
+        "pyright", "ruff", "black", "isort",
   		},
   	},
+  },
+
+  { "nvimtools/none-ls.nvim" },
+
+  {
+    "karloskar/poetry-nvim",
+    config = function()
+        require("poetry-nvim").setup()
+    end
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts) require'lsp_signature'.setup(opts) end
+  },
+
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup()
+    end,
   },
 
   {
@@ -38,7 +63,7 @@ return {
   	opts = {
   		ensure_installed = {
   			"vim", "lua", "vimdoc",
-       "html", "css"
+        "html", "css"
   		},
   	},
   },
