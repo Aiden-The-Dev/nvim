b950e75147fc0b49c03d9e4312b85cbc424797dc
Aiden <155806565+Aiden-The-Dev@users.noreply.github.com>
2024-06-18 04:47:37 +0300

    auto poetry env

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -32,3 +32,15 @@ lspconfig.tsserver.setup {
   on_init = on_init,
   capabilities = capabilities,
 }
+
+-- auto python poetry shell
+lspconfig.pyright.setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+  on_new_config = function(config, root_dir)
+    local env = vim.trim(vim.fn.system('cd "' .. root_dir .. '"; poetry env info -p 2>/dev/null'))
+    if string.len(env) > 0 then
+      config.settings.python.pythonPath = env .. "/bin/python"
+    end
+  end,
+}
